2d52961bec385d8e39f6e2f9f9ae3fe979f4b184
ademir <ademir@promaks.com.tr>
2022-10-27 02:40:34 +0300

    GulpFull

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const fileinclude = require("gulp-file-include");
+var cssmin = require("gulp-cssmin");
+var rename = require("gulp-rename");
+const gulp = require("gulp");
+var uglifycss = require("gulp-uglifycss");
+var importCss = require("gulp-import-css");
+const sass = require("gulp-sass")(require("sass"));
+const minify = require("gulp-minify");
+var webServer = require("gulp-server-livereload");
+var plumber = require('gulp-plumber'); // prevent server craching when get an error
+gulp.task("import", function (done) {
+  gulp.src(["src/js/index.js"]).pipe(minify()).pipe(gulp.dest("dist"));
+  done();
+});
+
+gulp.task("include",function (done) {
+  gulp
+    .src(["src/index.html"])
+    .pipe(plumber())
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
+  done();
+});
+
+gulp.task("css", function (done) {
+  gulp
+    .src("src/styles/index.css")
+    .pipe(importCss())
+    .pipe(cssmin())
+    // .pipe(rename({ suffix: ".min" }))
+    .pipe(
+      uglifycss({
+        maxLineLen: 80,
+        uglyComments: true,
+      })
+    )
+    .pipe(gulp.dest("dist"));
+  done();
+});
+
+gulp.task("scss", function (done) {
+  gulp
+    .src("./src/scss/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/scss"));
+
+  done();
+});
+
+gulp.task("webserver", function () {
+  gulp.src("dist").pipe(
+    webServer({
+      enable: true,
+      livereload: true,
+      //  directoryListing: true,
+     
+      open: true,
+      port: 8888,
+    })
+  );
+});
+gulp.task("bootstrap", function (done) {
+  gulp.src(["src/bootstrap/**"]).pipe(gulp.dest("dist/bootstrap"));
+  done();
+});
+
+gulp.task("watch", function (done) {
+  gulp.series(
+    gulp.task("import"),
+    gulp.task("bootstrap"),
+    gulp.task("include"),
+    gulp.task("css"),
+    gulp.task("scss"),
+    gulp.task("webserver")
+  )(); // call all tasks for the first time
+
+  gulp.watch(
+    ["src/**"],
+    gulp.series("include", "css", "scss", "import"),
+    function (file) {
+     
+    }
+  );
+  done();
+});
